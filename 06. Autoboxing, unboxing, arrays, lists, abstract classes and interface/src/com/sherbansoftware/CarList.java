252dc006d66f9ffec59d213c6203ab467907f894
Sherban Software <sherbansoftware@gmail.com>
2017-06-29 20:29:56 +0300

    modified:   06. Autoboxing, unboxing, arrays, lists, abstract classes and interface/.idea/workspace.xml

--- 06. Autoboxing, unboxing, arrays, lists, abstract classes and interface/src/com/sherbansoftware/CarList.java
+++ 06. Autoboxing, unboxing, arrays, lists, abstract classes and interface/src/com/sherbansoftware/CarList.java
@@ -28,15 +28,31 @@ public class CarList {
         System.out.println("The car object count is: " + carLinkedList.size());
     }
 
-    //modify item
+    //modify item without an index, call the modify method with index and object as param
+    public void modifyItem(Car newCar) {
+        int position = findItem(newCar, carLinkedList);
+        if (position >= 0) {
+            modifyItem(position, newCar);
+        }
+    }
+
+    //modify item with an index
     public void modifyItem(int index, Car newCar) {
         carLinkedList.set(index, newCar);
         int position = index + 1;
         System.out.println("Car index " + position + " has been modified with: " + getItem(index).getModel());
     }
 
-    //remove a car object from list
-    public void removeItem(int position, Car car) {
+    //remove a car object from list based on object, call the removeItem method based on an index
+    public void removeItem(Car carToBeRemoved) {
+        int position = findItem(carToBeRemoved, carLinkedList);
+        if (position >= 0) {
+            removeItem(position);
+        }
+    }
+
+    //remove a car object from list based on it's index
+    public void removeItem(int position) {
         Car carToRemove = carLinkedList.get(position);
         carLinkedList.remove(carToRemove);
     }
@@ -55,20 +71,27 @@ public class CarList {
         System.out.println("The list object contains " + list.size() + " car objects");
         //Traversing list through for
         for (int i = 0; i < list.size(); i++) {
-            System.out.println("Model: " + getCarLinkedList().get(i).getModel() + ". Manufacturer: " + getCarLinkedList().get(i).getManufacturer());
+            System.out.println("\t Car: " + (i + 1) + " - Model: " + getCarLinkedList().get(i).getModel() + ". Manufacturer: " + getCarLinkedList().get(i).getManufacturer());
         }
     }
 
-    public void findItem(Car searchedCar) {
+    //find item and return an object
+    public Car findItem(Car searchedCar) {
         boolean exists = carLinkedList.contains(searchedCar);
-
+        int position = 0;
         if (exists) {
-            int position = carLinkedList.indexOf(searchedCar) + 1;
-            System.out.println("Found car object at position: " + position);
+            position = carLinkedList.indexOf(searchedCar) + 1;
+            System.out.println("\t Found car object at position: " + position);
         } else {
-            System.out.println("Car object not found");
+            System.out.println("\t Car object not found");
         }
-
+        return carLinkedList.get(position);
     }
+
+    //find item and return an index
+    public int findItem(Car searchedCar, List list) {
+        return list.indexOf(searchedCar);
+    }
+
 }
 
--- 06. Autoboxing, unboxing, arrays, lists, abstract classes and interface/src/com/sherbansoftware/Main.java
+++ 06. Autoboxing, unboxing, arrays, lists, abstract classes and interface/src/com/sherbansoftware/Main.java
@@ -1,20 +1,68 @@
 package com.sherbansoftware;
 
+import java.util.Scanner;
+
 /**
  * Java array is an object the contains elements of similar data type. It is a data structure where we store similar elements. We can store only fixed set of elements in a java array.
  * Eg. array of characters, array of integer, array of String, array of SomeObject
  */
 public class Main {
     private static CarList carList = new CarList();
+    private static Scanner scanner = new Scanner(System.in);
 
     // static method which return the average of array elements
-    public static double getAvgOfArrElements(int[] arr) {
+    private static double getAvgOfArrElements(int[] arr) {
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
         }
         return (double) sum / (double) arr.length;
     }
+
+
+    private static void listItems(){
+        carList.listItems(carList.getCarArrayList());
+    }
+
+    private static void addItem() {
+        carList.addItem(new Car("Car to be added", "Model to be added"));
+    }
+
+    private static void modifyItem() {
+        System.out.print("Enter item number: ");
+        int itemNo = scanner.nextInt();
+        scanner.nextLine();
+        carList.modifyItem(itemNo - 1, new Car("Car to be modified", "Model to be modified"));
+    }
+
+    private static void removeItem() {
+        System.out.print("Enter item number: ");
+        int itemNo = scanner.nextInt();
+        scanner.nextLine();
+        carList.removeItem(itemNo - 1);
+
+    }
+
+    private static void searchForItem() {
+        Car searchedCar = new Car("Dacia", "Berlina");
+        if (carList.findItem(searchedCar) != null) {
+            System.out.println("Found " + searchedCar.getManufacturer() + " " + searchedCar.getModel() + " in  our car list");
+        } else {
+            System.out.println(searchedCar.getManufacturer() + " " + searchedCar.getModel() + " is not in the car list");
+        }
+    }
+
+    private static void printinstructions() {
+        System.out.println("\nPress ");
+        System.out.println("\t 0 - To print choice options.");
+        System.out.println("\t 1 - To print the list of car items.");
+        System.out.println("\t 2 - To add a car item to the list.");
+        System.out.println("\t 3 - To modify a car item in the list.");
+        System.out.println("\t 4 - To remove a car item from the list.");
+        System.out.println("\t 5 - To search for a car item in the car list.");
+        System.out.println("\t 6 - To quit the application.");
+    }
+
 
     public static void main(String[] args) {
         int[] arrayOfInt = new int[5];//declaration and instantiation, space for 5 elements. [] may be after or before variable name.
@@ -68,6 +116,43 @@ public class Main {
 
         carList.populateArrayList();
 
+        carList.removeItem(3);
+
         carList.listItems(carList.getCarArrayList());
+
+        boolean quit = false;
+        int choice = 0;
+        printinstructions();
+
+        while (!quit) {
+            System.out.println("Enter your choice");
+            choice = scanner.nextInt();
+            scanner.nextLine(); //clear the input buffer
+
+            switch (choice) {
+                case 0:
+                    printinstructions();
+                    break;
+                case 1:
+                    listItems();
+                    break;
+                case 2:
+                    addItem();
+                    break;
+                case 3:
+                    modifyItem();
+                    break;
+                case 4:
+                    removeItem();
+                    break;
+                case 5:
+                    searchForItem();
+                    break;
+                case 6:
+                    System.out.println("Exit switch!");
+                    quit = true;
+                    break;
+            }
+        }
     }
 }
