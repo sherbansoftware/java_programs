b7b53e2040568bb26f1320ce2ff3b454eeeda75e
Sherban Software <sherbansoftware@gmail.com>
2017-07-13 21:54:26 +0300

    19. Lambda expressions/Lambda expressions.iml

---
+++ 19. Lambda expressions/src/com/sherbansoftware/Main.java
@@ -0,0 +1,10 @@
+package com.sherbansoftware;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        ConcreteClass concreteClass = new ConcreteClass();
+        concreteClass.doStuff();
+    }
+}
